1696698d348771cffdd4fa7f043aa74fb910f79b
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-01 14:00:34 +0530

    mysql connection code

---
+++ mysql/cli/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+const config = require("./config/config.json")
+const connection = mysql.createConnection(config.dbcofig.mysql);
+
+
+// const connection = mysql.createConnection({
+//     host:"localhost",
+//     port:"3306",
+//     user:"root",
+//     password:"",
+//     database:"", //no dtatabases selected
+// });
+
+try {
+    connection.connect();
+    console.log('Connection Created Successfully!')
+} catch (error) {
+    console.log('Connection Error',error);
+}
+
+connection.end();
