5109daf75a4f9d9f03a2ec82fe6a413c52ccd1a0
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-14 09:30:00 +0530

    express notes 14-03-2023

--- express/app5/index.js
+++ express/app5/index.js
@@ -1,17 +1,30 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || '8080';
+// const express = require("express");
+// const app = express();
+// const PORT = process.env.PORT || '8080';
 
-app.listen(PORT, ()=>{
-    console.log(`Server Started on PORT ${PORT}`)
-});
+// app.listen(PORT, ()=>{
+//     console.log(`Server Started on PORT ${PORT}`)
+// });
 
-app.get('/', (req,res)=>{
-    res.send("Hello from Express");
-    res.send("Hello from Express");
-})
+// app.get('/', (req,res)=>{
+//     res.send("Hello from Express");
+//     res.send("Hello from Express");
+// })
 
-app.get('/users', (req,res)=>{
-    res.write("Hello from Express Users");
-    res.end();
+// app.get('/users', (req,res)=>{
+//     res.write("Hello from Express Users");
+//     res.end();
+// })
+
+// Updated version
+
+const http= require('http');
+const app = require('./app');
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || '8080';
+
+server.listen(port, ()=>{
+    console.log(`Server Started on port ${port}`);
 })
