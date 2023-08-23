d15a52840db65c9a88573f827d38ab38a26636c0
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2023-08-23 23:32:01 +0530

    Update auth.js

--- mysql-with-node/Node-crud/auth.js
+++ mysql-with-node/Node-crud/auth.js
@@ -13,7 +13,7 @@ const mysqlConnection = mysql.createConnection({
     port: '3306',
     user: 'root',
     password: '',
-    database: 'appwork'
+    database: ''
 });
 
 mysqlConnection.connect((err) =>
